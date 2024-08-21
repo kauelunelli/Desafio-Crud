22038d041e2244214c0a2ea6039b750111c00bab
Kaue Lunelli <lunellikaue@gmail.com>
2024-08-21 15:26:39 -0300

    feat: Add IPerson interface to web-react
    This commit adds the IPerson interface to the web-react project. The `IPerson.ts` file is created in the `src/interface` directory and includes properties for representing a person's information such as cpf, name, rg, zipCode, address, number, neighborhood, complement, city, and state. This interface will be used for defining the structure of person objects throughout the application.

---
+++ web-react/src/interface/IPerson.ts
@@ -0,0 +1,15 @@
+
+export interface IPerson {
+  id?: string;
+  cpf: string;
+  name: string;
+  rg: string;
+  zipCode: string;
+  address: string;
+  number: number,
+  neighborhood: string,
+  complement: string,
+  city: string,
+  state: string,
+
+}
