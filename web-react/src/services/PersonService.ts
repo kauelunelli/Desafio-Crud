1fd9d8e7a78e04e5cb8d645c5f61955136ee47dd
Kaue Lunelli <lunellikaue@gmail.com>
2024-08-22 00:53:46 -0300

    feat: Update PersonService to use axios for API requests
    This commit updates the PersonService in the web-react project to use the axios library for making API requests. The `PersonService.ts` file is modified to import the `api` instance from the `lib/axios` module and use it for sending GET, POST, PUT, and DELETE requests. This change improves the codebase by centralizing the API communication logic and ensures consistent handling of authorization headers.

--- web-react/src/services/PersonService.ts
+++ web-react/src/services/PersonService.ts
@@ -1,1 +1,74 @@
+import { removeMask } from "../lib/mask";
+import { IPerson } from "../interface/IPerson";
+import { api } from "../lib/axios";
 
+interface IGetPersons {
+
+  name?: string;
+  cpf?: string;
+  limit?: number;
+  offset?: number;
+}
+
+export const getPersons = async (params?: IGetPersons) => {
+  try {
+    const response = await api.get("/get-person", {
+      headers: {
+        Authorization: `Bearer ${localStorage.getItem("token")}`,
+      },
+      params: {
+        name: params?.name,
+        cpf: params?.cpf,
+        limit: params?.limit,
+        offset: params?.offset,
+      },
+    });
+
+    return response.data.person;
+  } catch (error) {
+    throw new Error("An error occurred" + error);
+  }
+};
+
+export const createPerson = async (person: IPerson) => {
+  person.cpf = removeMask(person.cpf);
+  try {
+    const response = await api.post("/create-person", person, {
+      headers: {
+        Authorization: `Bearer ${localStorage.getItem("token")}`,
+      },
+    });
+
+    return response.data.person;
+  } catch (error) {
+    throw new Error("An error occurred" + error);
+  }
+}
+
+export const updatePerson = async (person: IPerson) => {
+  try {
+    const response = await api.put("/update-person", person, {
+      headers: {
+        Authorization: `Bearer ${localStorage.getItem("token")}`,
+      },
+    });
+
+    return response.data.person;
+  } catch (error) {
+    throw new Error("An error occurred" + error);
+  }
+}
+
+export const deletePerson = async (id: string) => {
+  try {
+    await api.delete(`/remove-person/${id}`, {
+      headers: {
+        Authorization: `Bearer ${localStorage.getItem("token")}`,
+      },
+    });
+
+    window.location.reload();
+  } catch (error) {
+    throw new Error("An error occurred" + error);
+  }
+}
