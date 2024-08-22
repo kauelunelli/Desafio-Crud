7afee87d04416fe86ad4970de097709433af8676
Kaue Lunelli <lunellikaue@gmail.com>
2024-08-22 00:53:26 -0300

    feat: Add Modal component to web-react
    This commit adds the Modal component to the web-react project. The `modal.tsx` file is created in the `src/components` directory and includes functionality for displaying a modal dialog with a title, subtitle, and customizable content. This component enhances the user experience by providing a flexible and interactive way to present information or gather user input.

---
+++ web-react/src/components/modal.tsx
@@ -0,0 +1,47 @@
+import { X } from "lucide-react";
+import React from "react";
+
+interface ModalProps {
+  isOpen: boolean;
+  title: string;
+  subtitle?: string;
+  onClose: () => void;
+  children: React.ReactNode;
+}
+
+export function Modal({
+  isOpen,
+  title,
+  subtitle,
+  onClose,
+  children,
+}: ModalProps) {
+  const handleClose = () => {
+    onClose();
+  };
+
+  if (!isOpen) {
+    return null;
+  }
+
+  return (
+    <div
+      tabIndex={-1}
+      className={`fixed inset-0 bg-black/60 flex items-center justify-center z-50`}
+    >
+      <div className="space-y-2 w-full">
+        <div className="flex items-center justify-between">
+          <h3 className="font-lg font-semibold">{title}</h3>
+          <X
+            className="end-2.5 rounded-lg hover:bg-gray-600 bg-transparent hover:text-zinc-400 transition-colors duration-300  cursor-pointer"
+            onClick={handleClose}
+          />
+        </div>
+        <p className="text-sm text-zinc-400">{subtitle}</p>
+        <div className="pt-4">
+          <div className="space-y-4">{children}</div>
+        </div>
+      </div>
+    </div>
+  );
+}
