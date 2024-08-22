8c6cebc063b2c6a2dedde66dfac71e9a8afa396e
Kaue Lunelli <lunellikaue@gmail.com>
2024-08-22 17:49:23 -0300

    feat: Update Modal component in web-react
    The Modal component in the web-react project is updated to improve accessibility and styling. The `modal.tsx` file is modified to add a background color and adjust the font color for better readability. This change enhances the user experience by making the modal more visually appealing and easier to use.

--- web-react/src/components/modal.tsx
+++ web-react/src/components/modal.tsx
@@ -27,19 +27,23 @@ export function Modal({
   return (
     <div
       tabIndex={-1}
-      className={`fixed inset-0 bg-black/60 flex items-center justify-center z-50`}
+      className={` fixed inset-0 bg-black/60 flex items-center justify-center z-50`}
     >
-      <div className="space-y-2 w-full">
-        <div className="flex items-center justify-between">
-          <h3 className="font-lg font-semibold">{title}</h3>
-          <X
-            className="end-2.5 rounded-lg hover:bg-gray-600 bg-transparent hover:text-zinc-400 transition-colors duration-300  cursor-pointer"
-            onClick={handleClose}
-          />
-        </div>
-        <p className="text-sm text-zinc-400">{subtitle}</p>
-        <div className="pt-4">
-          <div className="space-y-4">{children}</div>
+      <div className="bg-gray-900 max-w-xl rounded-xl py-5 px-6 ">
+        <div className="space-y-3 w-full">
+          <div className="flex items-center justify-between">
+            <h3 className="font-lg font-semibold text-gray-900 dark:text-white">
+              {title}
+            </h3>
+            <X
+              className="end-2.5 rounded-lg text-white hover:bg-gray-600 bg-transparent hover:text-zinc-400 transition-colors duration-300  cursor-pointer"
+              onClick={handleClose}
+            />
+          </div>
+          <p className="text-sm text-gray-900 dark:text-white">{subtitle}</p>
+          <div className="pt-4">
+            <div className="space-y-4">{children}</div>
+          </div>
         </div>
       </div>
     </div>
