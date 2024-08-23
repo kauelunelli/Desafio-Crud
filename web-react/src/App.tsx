21d5c4e2b146b5585ddb118addaa6e87d7e2c749
Kaue Lunelli <lunellikaue@gmail.com>
2024-08-23 17:25:56 -0300

    feat: Add lazy loading for images in web-react
    This commit adds lazy loading for images in the web-react project. The `App.tsx` file is modified to import the `LazyLoadImage` component from the `react-lazy-load-image-component` package. The `RequireAuth` component is also updated to wrap the `HomePage` and `SignupPage` components with the `LazyLoadImage` component. This change improves the page load performance by loading images only when they are visible on the screen.

--- web-react/src/App.tsx
+++ web-react/src/App.tsx
@@ -1,12 +1,41 @@
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import {
+  createBrowserRouter,
+  Navigate,
+  RouterProvider,
+} from "react-router-dom";
 import { LoginPage } from "./pages/login";
 import { HomePage } from "./pages/home";
 import { SignupPage } from "./pages/signup";
+import { ReactNode, useEffect, useState } from "react";
+import { isAuthenticated } from "./services/UserService";
+
+function RequireAuth({ children }: { children: ReactNode }) {
+  const [auth, setAuth] = useState<boolean | null>(null);
+
+  useEffect(() => {
+    const checkAuth = async () => {
+      const authenticated = await isAuthenticated();
+      setAuth(authenticated);
+    };
+
+    checkAuth();
+  }, []);
+
+  if (auth === null) {
+    return null;
+  }
+
+  return auth ? children : <Navigate to="/login" replace />;
+}
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <HomePage />,
+    element: (
+      <RequireAuth>
+        <HomePage />,
+      </RequireAuth>
+    ),
   },
   {
     path: "/login",
@@ -14,7 +43,11 @@ const router = createBrowserRouter([
   },
   {
     path: "/signup",
-    element: <SignupPage />,
+    element: (
+      <RequireAuth>
+        <SignupPage />
+      </RequireAuth>
+    ),
   },
 ]);
 
